a4a859aeca1cf62860d657b43a788d86982f5394
John Vilsack <vilsack@gmail.com>
2025-06-29 20:30:16 -0500

    debug

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ echo "3 Installing dotfiler to $INSTALL_DIR..."
 
 # Clone or download
 if command -v git >/dev/null 2>&1; then
+    echo "Git found, cloning repository..."
     git clone "$REPO_URL" /tmp/dotfiler
     cd /tmp/dotfiler
 else
@@ -17,15 +18,20 @@ else
     exit 1
 fi
 
-# Create install directory
+# Create install 
+echo "Creating install directory..."
 mkdir -p "$INSTALL_DIR"
 
 # Copy files
+echo "Copying files to $INSTALL_DIR..."
 cp dotfiler/dotfiler "$INSTALL_DIR/"
+echo "Copying dotfiler-lib to $INSTALL_DIR..."
 cp -rf dotfiler/dotfiler-lib "$INSTALL_DIR/"
+echo "Make X to $INSTALL_DIR..."
 chmod +x "$INSTALL_DIR/dotfiler"
 
 # Cleanup
+echo "Cleaning up..."
 rm -rf /tmp/dotfiler
 
 echo "dotfiler installed!"
