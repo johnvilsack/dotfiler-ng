b344de41bf619d6d5164bcd799a8bc6a90de47e3
John Vilsack <vilsack@gmail.com>
2025-06-30 09:01:16 -0500

    added hash write for update checks

--- install.sh
+++ install.sh
@@ -24,6 +24,13 @@ mkdir -p "$INSTALL_DIR"
 # Copy files
 cp /tmp/dotfiler/dotfiler "$INSTALL_DIR/" && cp -rf /tmp/dotfiler/dotfiler-lib "$INSTALL_DIR/"
 chmod +x "$INSTALL_DIR/dotfiler"
+
+# Get hash of latest commit and add for future update checks
+DOTFILER_HASH_FILE="$HOME/.local/.dotfiler_last_hash"
+DOTFILER_CURRENT_HASH=$(curl -s https://api.github.com/repos/johnvilsack/dotfiler/commits/HEAD | grep '"sha"' | head -1 | cut -d'"' -f4)
+echo "$DOTFILER_CURRENT_HASH" > "$DOTFILER_HASH_FILE"
+echo "Current commit hash: $DOTFILER_CURRENT_HASH"
+
 # Cleanup
 rm -rf /tmp/dotfiler
 
